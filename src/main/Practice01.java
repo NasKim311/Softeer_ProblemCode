4f1347f4de1ae47647094de751d90b646837e7ab
Nas <kimna0311@naver.com>
2023-02-02 15:36:09 +0900

    practice01_done

---
+++ src/main/Practice01.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.util.Scanner;
+
+public class Practice01 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String distanceDriven = sc.nextLine();
+
+        String[] distanceArr = distanceDriven.split(" ");
+        int distanceA = Integer.parseInt(distanceArr[0]);
+        int distanceB = Integer.parseInt(distanceArr[1]);
+
+        if(distanceA > distanceB){
+            System.out.println("A");
+        }else if(distanceA < distanceB){
+            System.out.println("B");
+        } else{
+            System.out.println("same");
+        }
+	}
+}
