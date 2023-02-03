d4e28c10b402aea2202507e025d9c108e563e29c
Nas <kimna0311@naver.com>
2023-02-03 17:12:53 +0900

    practice04_proceeding

---
+++ src/main/Practice04.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.*;
+import java.io.*;
+
+public class Practice04 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int participantCnt = 0;
+		
+		
+		
+		
+	}
+}
