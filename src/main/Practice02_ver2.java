f2bbc92e47948fe39fc23311189008ccc7b8008b
Nas <kimna0311@naver.com>
2023-02-06 16:45:01 +0900

    practice02_modified

--- src/main/Practice02_ver2.java
+++ src/main/Practice02_ver2.java
@@ -17,17 +17,59 @@ public class Practice02_ver2 {
 		String THUWorkingTime = sc.nextLine();
 		String FRIWorkingTime = sc.nextLine();
 
-		// 모든 근무시간의 값을 넣은 배열 선언
-		String[] ALLWorkingTime = {MONWorkingTime, TUEWorkingTime, WEDWorkingTime, THUWorkingTime, FRIWorkingTime};
-		List<String[]> Start_EndTime = new ArrayList<>();
+		// 모든 근무시간의 값을 넣은 배열 선언		
+		//	ex) ALLWorkingTime = {10:00 19:00 , 09:00 15:00 , ...}
 		
-		for(int i = 0; i < ALLWorkingTime.length; i++) {
+		String[] ALLWorkingTime = { MONWorkingTime, TUEWorkingTime, WEDWorkingTime, THUWorkingTime, FRIWorkingTime };
+
+		// 출근시간과 퇴근시간을 쪼갠 값이 들어간 배열을 담을 list 선언 (리스트의 값은 월~금요일 순서)	
+		//	ex) Start_EndTime = [ {10:00 , 19:00} , {09:00 , 15:00} ,... ]
+		
+		List<String[]> Start_EndTime = new ArrayList<>();
+
+		for (int i = 0; i < ALLWorkingTime.length; i++) {
 			Start_EndTime.add(ALLWorkingTime[i].split(" "));
 		}
+
+		// 출근시간과 퇴근시간 각각 시,분으로 쪼갠 값이 들어간 배열을 담을 list 선언	(리스트의 값은 월~금요일 순서)	
+		//	ex) Start_HourMinute = [{10 , 00} , {09 , 00} , ...]
+		//		End_HourMinute	 = [{19 , 00} , {15 , 00} , ...]
+		
+		List<String[]> Start_HourMinute = new ArrayList<>();
+		List<String[]> End_HourMinute = new ArrayList<>();
+		
 		
 		for (int i = 0; i < Start_EndTime.size(); i++) {
-			System.out.println(Start_EndTime.get(i)[i]);
+//			System.out.println(Start_EndTime.size());
+//			System.out.println(Start_EndTime.get(i)[0]);
+//			System.out.println(Start_EndTime.get(i)[1]);
+			
+			Start_HourMinute.add(Start_EndTime.get(i)[0].split(":"));
+			End_HourMinute.add(Start_EndTime.get(i)[1].split(":"));
 		}
+		
+		int result = 0;
+		
+		for (int i = 0; i < Start_HourMinute.size(); i++) {
+			
+			// 각 요일의 출근시간의 '시'
+			int startHour = Integer.parseInt(Start_HourMinute.get(i)[0]);
+			int startMinute = Integer.parseInt(Start_HourMinute.get(i)[1]);
+			int endHour = Integer.parseInt(End_HourMinute.get(i)[0]);
+			int endMinute = Integer.parseInt(End_HourMinute.get(i)[1]);		
+			
+			// 퇴근시간의 분이 출근시간의 분 보다 클 때
+			if(endMinute - startMinute >= 0) {
+				result += (endHour - startHour) * 60 + (endMinute - startMinute);
+				
+			// 퇴근시간의 분이 출근시간의 분 보다 작을 때
+			} else {
+				result += (endHour - 1 - startHour) * 60 + (60 + endMinute - startMinute);
+			}
+			
+		}
+		
+		System.out.println(result);
 		
 	} // main method
 
